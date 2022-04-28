cd031831e82fe7ca8dd426a29e5f14c3298495c7
tylor moore <mooretylor28@gmail.com>
2022-04-28 09:46:03 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,39 @@
+
+let a = 20;
+let b = 4;
+let add = a + b;
+console.log(add);
+let multiply = a * b; 
+console.log(multiply);
+let dividing = a / b;
+console.log(dividing);
+
+let A = 11;
+let str = "11";
+let str2 = "eleven";
+let isPresent = true;
+let firstName = "Jackie";
+let lastName = "Chan";
+
+console.log(`A + str: ${A + str}`);
+console.log(`A + str2: ${A + str2}`);
+console.log(`A + isPresent: ${A + isPresent}`);
+console.log(`A + isPresent: ${A + isPresent}`);
+console.log(`A + lastName: ${A + lastName}`);
+
+let d = 5;
+let str3 = "5";
+let str4 = "five";
+let isPresent2 = false;
+let firstName2 = "Robin";
+let lastName2 = "Williams";
+
+console.log(`d == str3: ${d == str3}`);
+console.log(`d === str3: ${d === str3}`);
+console.log(`!isPresent2: ${!isPresent2}`);
+console.log(`"eleven" == str4 && d >= str3: ${"eleven" == str4 && d >= str3}`);
+console.log(`!isPresent2 || isPresent2: ${!isPresent2 || isPresent2}`);
+console.log(`0 == false: ${0 == false}`);
+console.log(`0 === false: ${0 === false}`);
+console.log(`0 != false: ${0 != false}`);
+console.log(`0 !== false: ${0 !== false}`);
